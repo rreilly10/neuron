9254000c72e81e6da3d80e6aa96f884787f9ec78
Robert Reilly <reillyr7@gmail.com>
2016-06-12 14:24:05 -0500

    Inital commit

---
+++ implementation.py
@@ -0,0 +1,22 @@
+class ANN:
+    """
+    Artificial Neural Network
+    """
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update_weights(self):
+        pass
+
+
+class Layer:
+
+    def __init__(self):
+        pass
